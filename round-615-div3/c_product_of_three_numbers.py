8a2762c2495254b67294327703092016d166485f
Eloy Hernandez <eloyhz@gmail.com>
2020-01-22 11:54:09 -0600

    Product of three numbers with time limit exceeded

--- round-615-div3/c_product_of_three_numbers.py
+++ round-615-div3/c_product_of_three_numbers.py
@@ -6,15 +6,25 @@ if __name__ == '__main__':
 	for x in range(t):
 		# print("Case", x + 1)
 		n = int(input())
+		nn = n
 		i = 2
 		div = []
-		while n >= i and n > 1 and len(div) < 3:
+		while n >= i and len(div) < 2:
 			if n % i == 0:
 				n = n // i
-				div.append(str(i))
+				div.append(i)
 			i += 1
-		if n == 1 and len(div) == 3:
-			print("YES")
-			print(' '.join(div), '')
+		if len(div) == 2:
+			a = div[0]
+			b = div[1]
+			if nn % (a * b) != 0:
+				print("NO")
+			else:
+				c = nn // (a * b)
+				if c <= b:
+					print("NO")
+				else:
+					print("YES")
+					print(a, b, c, '')
 		else:
 			print("NO")
